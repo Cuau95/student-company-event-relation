3e9b863abe7c16e631ef644738e53eae0f9bdf35
Cuau95 <elzua_ecm@live.com.mc>
2019-05-16 14:42:54 -0500

    add post endpoint

--- src/main/java/com/example/student/company/event/relation/infrastruture/controller/StudentEventCompanyRelationController.java
+++ src/main/java/com/example/student/company/event/relation/infrastruture/controller/StudentEventCompanyRelationController.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.student.company.event.relation.infrastruture.controller;
 
+import com.example.student.company.event.relation.domain.model.StudentEventCompanyRelation;
+import com.example.student.company.event.relation.domain.service.StudentEventCompanyRelationService;
+import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/student-company-event-relation")
 public class StudentEventCompanyRelationController {
+    
+    private StudentEventCompanyRelationService service;
+
+    public StudentEventCompanyRelationController(StudentEventCompanyRelationService service) {
+        this.service = service;
+    }
+    
+    @PostMapping("/student/{idStudent}/company/id/{idCompany}/event/id/{idEvent}")
+    public ResponseEntity<StudentEventCompanyRelation> saveStudentEventCompanyRelation(
+            @PathVariable String idStudent, @PathVariable String idCompany, @PathVariable String idEvent
+    ) {
+        StudentEventCompanyRelation relationSaved = service.saveStudentEventCompanyRelation(
+                idStudent, idCompany, idEvent
+        );
+        HttpStatus status = OK;
+        if(
+                relationSaved.getAlumno() == null 
+                || relationSaved.getEmpresa() == null 
+                || relationSaved.getFeriaEmpleo() == null
+           ){
+            status = NOT_FOUND;
+        }
+        return new ResponseEntity<>(relationSaved, status);
+    }
     
 }
