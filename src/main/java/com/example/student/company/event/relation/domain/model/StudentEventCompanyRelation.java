e96132aab43adb0c474bf9f03d3b81083ac4e4d8
Cuau95 <elzua_ecm@live.com.mc>
2019-05-16 14:29:13 -0500

    add getters and setters

--- src/main/java/com/example/student/company/event/relation/domain/model/StudentEventCompanyRelation.java
+++ src/main/java/com/example/student/company/event/relation/domain/model/StudentEventCompanyRelation.java
@@ -10,5 +10,45 @@ public class StudentEventCompanyRelation implements Serializable{
     private Company empresa;
     private Event feriaEmpleo;
     private Date fecha;
+
+    public int getIdRelacion() {
+        return idRelacion;
+    }
+
+    public void setIdRelacion(int idRelacion) {
+        this.idRelacion = idRelacion;
+    }
+
+    public Student getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Student alumno) {
+        this.alumno = alumno;
+    }
+
+    public Company getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Company empresa) {
+        this.empresa = empresa;
+    }
+
+    public Event getFeriaEmpleo() {
+        return feriaEmpleo;
+    }
+
+    public void setFeriaEmpleo(Event feriaEmpleo) {
+        this.feriaEmpleo = feriaEmpleo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
     
 }
